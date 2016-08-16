019720a8a8757b5c44ec01f26596f25178fd74f1
PaulHancock <paul.hancock@curtin.edu.au>
2016-08-16 16:54:43 +0800

    added plotting functionality Plots will show dra/ddec/offset/angle

--- fits_warp.py
+++ fits_warp.py
@@ -3,7 +3,9 @@
 __author__ = "PaulHancock"
 __date__ = "11/08/2016"
 
+import math
 import numpy as np
+import os
 from scipy import interpolate
 from scipy.interpolate import LinearNDInterpolator
 from astropy import wcs
@@ -14,7 +16,7 @@ import sys
 import argparse
 
 
-def make_models(fname, ra1='ra', dec1='dec', ra2='RAJ2000', dec2='DEJ2000'):
+def make_models(fname, ra1='ra', dec1='dec', ra2='RAJ2000', dec2='DEJ2000', plots=False):
     """
     Read a fits file which contains the crossmatching results for two catalogues.
     Catalogue 1 is the source catalogue (positions that need to be corrected)
@@ -25,6 +27,7 @@ def make_models(fname, ra1='ra', dec1='dec', ra2='RAJ2000', dec2='DEJ2000'):
     :param dec1: column name for the dec degrees in catalogue 1 (source)
     :param ra2: column name for the ra degrees in catalogue 2 (reference)
     :param dec2: column name for the dec degrees in catalogue 2 (reference)
+    :param plots: True = Make plots
     :return: (dramodel, ddecmodel)
     """
     raw_data = fits.open(fname)[1].data
@@ -42,6 +45,59 @@ def make_models(fname, ra1='ra', dec1='dec', ra2='RAJ2000', dec2='DEJ2000'):
 
     dramodel = interpolate.Rbf(catalog[0].degree, catalog[1].degree, dra, function='linear', smooth=3)
     ddecmodel = interpolate.Rbf(catalog[0].degree, catalog[1].degree, ddec, function='linear', smooth=3)
+
+    if plots:
+        from matplotlib import pyplot
+        xmin,xmax = math.floor(np.nanmin(catalog[0].degree)), math.ceil(np.nanmax(catalog[0].degree))
+        ymin,ymax = math.floor(np.nanmin(catalog[1].degree)), math.ceil(np.nanmax(catalog[1].degree))
+
+        smax = np.max(np.hypot(dra, ddec))*3600
+
+        fig = pyplot.figure(figsize=(28, 7))
+
+        ax = fig.add_subplot(1, 2, 1)
+        ax.quiver(X, Y, 60*dra, 60*ddec, np.arctan2(dra,ddec), angles='xy', scale_units='xy', scale=1)
+        ax.set_title("source vectors")
+
+        ax = fig.add_subplot(1, 2, 2)
+        ax.quiver(vunwrap(gridX), gridY, 60*U, 60*V, np.arctan2(U,V), angles='xy', scale_units='xy', scale=1)
+        ax.set_title("model vectors")
+
+        ax = fig.add_subplot(1, 4, 1)
+        cax = ax.scatter(catalog[0].degree, catalog[1].degree, c=dra*3600,
+                         edgecolor='', vmin=-1*smax, vmax=smax)
+        ax.set_xlim((xmin, xmax))
+        ax.set_ylim((ymin, ymax))
+        ax.set_title("DRA")
+        cbar = fig.colorbar(cax, orientation='horizontal')
+
+        ax = fig.add_subplot(1, 4, 2)
+        cax = ax.scatter(catalog[0].degree, catalog[1].degree, c=ddec*3600,
+                         edgecolor='', vmin=-1*smax, vmax=smax)
+        ax.set_xlim((xmin, xmax))
+        ax.set_ylim((ymin, ymax))
+        ax.set_title("DDec")
+        cbar = fig.colorbar(cax, orientation='horizontal')
+
+
+        ax = fig.add_subplot(1, 4, 3)
+        cax = ax.scatter(catalog[0].degree, catalog[1].degree, c=np.hypot(dra, ddec)*3600,
+                         edgecolor='', vmin=0, vmax=smax)
+        ax.set_xlim((xmin, xmax))
+        ax.set_ylim((ymin, ymax))
+        ax.set_title('offset')
+        cbar = fig.colorbar(cax, orientation='horizontal')
+
+        ax = fig.add_subplot(1, 4, 4)
+        cax = ax.scatter(catalog[0].degree, catalog[1].degree, c=np.degrees(np.arctan2(dra, ddec)),
+                         edgecolor='', vmin=-180, vmax=180)
+        ax.set_xlim((xmin, xmax))
+        ax.set_ylim((ymin, ymax))
+        ax.set_title('angle')
+        cbar = fig.colorbar(cax, orientation='horizontal')
+
+        outname = os.path.splitext(fname)[0] +'.png'
+        pyplot.savefig(outname, dpi=200)
 
     return dramodel, ddecmodel
 
@@ -117,7 +173,9 @@ if __name__ == "__main__":
                         help="The column name for ra  (degrees) for reference catalogue.")
     group2.add_argument("--dec2", dest='dec2', type=str, default='DEJ2000',
                         help="The column name for dec (degrees) for reference catalogue.")
-    group3 = parser.add_argument_group("testing")
+    group3 = parser.add_argument_group("Other")
+    group3.add_argument('--plot', dest='plot', default=False, action='store_true',
+                        help="Plot the ra/dec offset models")
     group3.add_argument('--test', dest='test', default=False, action='store_true',
                         help="run test")
 
@@ -132,5 +190,13 @@ if __name__ == "__main__":
         correct_image('small.fits', dra, ddec, 'small_corr.fits')
         sys.exit()
 
-    dra, ddec = make_models(results.xm, results.ra1, results.dec1, results.ra2, results.dec2)
-    correct_image(results.infits, dra, ddec, results.outfits)
+    if results.infits is not None or results.plot:
+        dra, ddec = make_models(results.xm, results.ra1, results.dec1, results.ra2, results.dec2, results.plot)
+
+    if results.infits is not None:
+        if os.path.exists(results.infits):
+            correct_image(results.infits, dra, ddec, results.outfits)
+        else:
+            print "File:{0} not found".format(results.infits)
+    else:
+        print "No fits file supplied not doing warping"
