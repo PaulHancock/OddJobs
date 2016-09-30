6b9c1c60fdb42b4eb842771b4dc67ac604b9b8be
PaulHancock <paul.hancock@curtin.edu.au>
2016-09-30 16:55:39 +0800

    incorporated changes from NHW - read fits or vot format catalogues - better plotting - new interpolator to conserve flux - ability to warp multiple images with the same model - more verbose help text - removed broken testing code - removed catalogue correction code

--- fits_warp.py
+++ fits_warp.py
@@ -1,19 +1,22 @@
 #! python
 
-__author__ = "PaulHancock"
-__date__ = "18/08/2016"
+__author__ = "Paul Hancock and Natasha Hurley-Walker"
+__date__ = "05/09/2016"
 
 import numpy as np
 import os
 from scipy import interpolate
-from scipy.interpolate import LinearNDInterpolator
+from scipy.interpolate import CloughTocher2DInterpolator
 from astropy import wcs
 from astropy.io import fits
+from astropy.io.votable import parse_single_table
 import sys
+import glob
 import argparse
 
 
-def make_pix_models(fname, ra1='ra', dec1='dec', ra2='RAJ2000', dec2='DEJ2000', fitsname=None, plots=False):
+def make_pix_models(fname, ra1='ra', dec1='dec', ra2='RAJ2000', dec2='DEJ2000', fitsname=None, plots=False,
+                    smooth=300.):
     """
     Read a fits file which contains the crossmatching results for two catalogues.
     Catalogue 1 is the source catalogue (positions that need to be corrected)
@@ -25,70 +28,130 @@ def make_pix_models(fname, ra1='ra', dec1='dec', ra2='RAJ2000', dec2='DEJ2000', 
     :param ra2: column name for the ra degrees in catalogue 2 (reference)
     :param dec2: column name for the dec degrees in catalogue 2 (reference)
     :param plots: True = Make plots
-    :return: (dramodel, ddecmodel)
+    :param smooth: smoothing radius (in pixels) for the RBF function
+    :return: (dxmodel, dymodel)
     """
-    raw_data = fits.open(fname)[1].data
+    filename, file_extension = os.path.splitext(fname)
+    if file_extension == ".fits":
+        raw_data = fits.open(fname)[1].data
+    elif file_extension == ".vot":
+        raw_data = parse_single_table(fname).array
 
     # get the wcs
     hdr = fits.getheader(fitsname)
     imwcs = wcs.WCS(hdr, naxis=2)
 
     # filter the data to only include SNR>10 sources
-    flux_mask = np.where(raw_data['peak_flux']/raw_data['local_rms']>10)
+    flux_mask = np.where(raw_data['peak_flux'] / raw_data['local_rms'] > 10)
     data = raw_data[flux_mask]
-
-    #calculate the offsets in the ra/dec directions
-    # catalog = Longitude(data[ra1], unit=u.degree), Latitude(data[dec1], unit=u.degree)
-    # reference = Longitude(data[ra2], unit=u.degree), Latitude(data[dec2], unit=u.degree)
 
     cat_xy = imwcs.all_world2pix(zip(data[ra1], data[dec1]), 1)
     ref_xy = imwcs.all_world2pix(zip(data[ra2], data[dec2]), 1)
 
     diff_xy = ref_xy - cat_xy
 
-    dxmodel = interpolate.Rbf(cat_xy[:, 0], cat_xy[:, 1], diff_xy[:, 0], function='linear', smooth=3)
-    dymodel = interpolate.Rbf(cat_xy[:, 0], cat_xy[:, 1], diff_xy[:, 1], function='linear', smooth=3)
+    dxmodel = interpolate.Rbf(cat_xy[:, 0], cat_xy[:, 1], diff_xy[:, 0], function='linear', smooth=smooth)
+    dymodel = interpolate.Rbf(cat_xy[:, 0], cat_xy[:, 1], diff_xy[:, 1], function='linear', smooth=smooth)
 
     if plots:
+        import matplotlib
+        # Super-computer-safe
+        matplotlib.use('Agg')
         from matplotlib import pyplot
+        from matplotlib import gridspec
+        # Perceptually uniform cyclic color schemes
+        try:
+            import seaborn as sns
+
+            cmap = matplotlib.colors.ListedColormap(sns.color_palette("husl", 256))
+        except ImportError:
+            cmap = 'hsv'
+        # Attractive serif fonts
+        try:
+            from matplotlib import rc
+
+            rc('text', usetex=True)
+            rc('font', **{'family': 'serif', 'serif': ['serif']})
+        except:
+            pass
         xmin, xmax = 0, hdr['NAXIS1']
         ymin, ymax = 0, hdr['NAXIS2']
 
-        gx, gy = np.mgrid[xmin:xmax:(xmax-xmin)/20., ymin:ymax:(ymax-ymin)/20.]
+        gx, gy = np.mgrid[xmin:xmax:(xmax - xmin) / 50., ymin:ymax:(ymax - ymin) / 50.]
         mdx = dxmodel(np.ravel(gx), np.ravel(gy))
         mdy = dymodel(np.ravel(gx), np.ravel(gy))
 
         x = cat_xy[:, 0]
         y = cat_xy[:, 1]
+
+        # plot w.r.t. centre of image, in degrees
+        try:
+            delX = abs(hdr['CD1_1'])
+        except:
+            delX = abs(hdr['CDELT1'])
+        try:
+            delY = hdr['CD2_2']
+        except:
+            delY = hdr['CDELT2']
+        # shift all co-ordinates and put them in degrees
+        x -= hdr['NAXIS1'] / 2
+        gx -= hdr['NAXIS1'] / 2
+        xmin -= hdr['NAXIS1'] / 2
+        xmax -= hdr['NAXIS1'] / 2
+        x *= delX
+        gx *= delX
+        xmin *= delX
+        xmax *= delX
+        y -= hdr['NAXIS2'] / 2
+        gy -= hdr['NAXIS2'] / 2
+        ymin -= hdr['NAXIS2'] / 2
+        ymax -= hdr['NAXIS2'] / 2
+        y *= delY
+        gy *= delY
+        ymin *= delY
+        ymax *= delY
+        scale = 1
+
         dx = diff_xy[:, 0]
         dy = diff_xy[:, 1]
 
-        fig = pyplot.figure(figsize=(14, 7))
-        kwargs = {'angles':'xy', 'scale_units':'xy', 'scale':1, 'cmap':'hsv'} #, 'vmin':-180, 'vmax':180}
+        fig = pyplot.figure(figsize=(12, 6))
+        gs = gridspec.GridSpec(100, 100)
+        gs.update(hspace=0, wspace=0)
+        kwargs = {'angles': 'xy', 'scale_units': 'xy', 'scale': scale, 'cmap': cmap, 'clim': [-180, 180]}
         angles = np.degrees(np.arctan2(dy, dx))
-        ax = fig.add_subplot(1, 2, 1)
-        cax = ax.quiver(x, y, dx*60, dy*60, angles, **kwargs)
+        ax = fig.add_subplot(gs[0:100, 0:48])
+        cax = ax.quiver(x, y, dx, dy, angles, **kwargs)
         ax.set_xlim((xmin, xmax))
         ax.set_ylim((ymin, ymax))
-        ax.set_title("Offsets")
-        cbar = fig.colorbar(cax, orientation='horizontal')
+        ax.set_xlabel("Distance from pointing centre / degrees")
+        ax.set_ylabel("Distance from pointing centre / degrees")
+        ax.set_title("Source position offsets / arcsec")
 
-        ax = fig.add_subplot(1, 2, 2)
-        cax = ax.quiver(gx, gy, mdx*60, mdy*60, np.degrees(np.arctan2(mdy, mdx)), **kwargs)
+        ax = fig.add_subplot(gs[0:100, 49:97])
+        cax = ax.quiver(gx, gy, mdx, mdy, np.degrees(np.arctan2(mdy, mdx)), **kwargs)
         ax.set_xlim((xmin, xmax))
         ax.set_ylim((ymin, ymax))
-        ax.set_title("model vectors")
-        cbar = fig.colorbar(cax, orientation='horizontal')
+        ax.set_xlabel("Distance from pointing centre / degrees")
+        ax.tick_params(axis='y', labelleft='off')
+        ax.set_title("Model position offsets / arcsec")
 
-        outname = os.path.splitext(fname)[0] +'.png'
+        # Color bar
+        ax2 = fig.add_subplot(gs[0:100, 98:100])
+        cbar3 = pyplot.colorbar(cax, cax=ax2, use_gridspec=True)
+        cbar3.set_label('Angle CCW from West / degrees')
+        cbar3.ax.yaxis.set_ticks_position('right')
+
+        outname = os.path.splitext(fname)[0] + '.png'
+        pyplot.show()
         pyplot.savefig(outname, dpi=200)
 
     return dxmodel, dymodel
 
 
-def correct_image(fname, dxmodel, dymodel, fout):
+def correct_images(fnames, dxmodel, dymodel, suffix):
     """
-    Read a fits image, and apply pixel-by-pixel corrections based on the
+    Read a list of fits image, and apply pixel-by-pixel corrections based on the
     given x/y models. Interpolate back to a regular grid, and then write an
     output file
     :param fname: input fits file
@@ -97,104 +160,64 @@ def correct_image(fname, dxmodel, dymodel, fout):
     :param fout: output fits file
     :return: None
     """
-    im = fits.open(fname)
+    # Get co-ordinate system from first image
+    im = fits.open(fnames[0])
     data = np.squeeze(im[0].data)
     # create a map of (x,y) pixel pairs as a list of tuples
     xy = np.indices(data.shape, dtype=np.float32)
-    xy.shape = (2, xy.shape[1]*xy.shape[2])
+    xy.shape = (2, xy.shape[1] * xy.shape[2])
 
     x = np.array(xy[1, :])
     y = np.array(xy[0, :])
 
     # calculate the corrections in blocks of 100k since the rbf fails on large blocks
-    print 'applying corrections',
+    print 'applying corrections to pixel co-ordinates',
     if len(x) > 100000:
         print 'in cycles'
-        borders = range(0, len(x)+1, 100000)
+        n = 0
+        borders = range(0, len(x) + 1, 100000)
         if borders[-1] != len(x):
             borders.append(len(x))
         for s1 in [slice(a, b) for a, b in zip(borders[:-1], borders[1:])]:
             x[s1] += dxmodel(x[s1], y[s1])
             # the x coords were just changed so we need to refer back to the original coords
             y[s1] += dymodel(xy[1, :][s1], y[s1])
+            n += 1
+            sys.stdout.write("{0:3.0f}%...".format(100 * n / len(borders)))
+            sys.stdout.flush()
     else:
         print 'all at once'
         x += dxmodel(x, y)
         y += dymodel(xy[1, :], y)
 
-    print 'interpolating'
-    ifunc = LinearNDInterpolator(np.transpose([x,y]), np.ravel(data))
-    interpolated_map = ifunc(xy[1, :], xy[0, :])
-
-    print 'writing'
-    interpolated_map.shape = data.shape
-    im[0].data = interpolated_map
-    im.writeto(fout, clobber=True)
-    print "wrote", fout
-    return
-
-
-def correct_catalogue(fname, dramodel, ddecmodel, fout):
-    """
-    Read a fits image, and apply pixel-by-pixel ra/dec corrections based on the
-    given dra/ddec models. Interpolate back to a regular grid, and then write an
-    output file
-    :param fname: input fits file
-    :param dramodel: ra model (in degrees)
-    :param ddecmodel: dec model (in degrees)
-    :param fout: output fits file
-    :return: None
-    """
-    im = fits.open(fname)
-    data = np.squeeze(im[0].data)
-    imwcs = wcs.WCS(im[0].header, naxis=2)
-    # create a map of (x,y) pixel pairs as a list of tuples
-    xy = np.indices(data.shape[-2:])
-    xy.shape = (2, xy.shape[1]*xy.shape[2])
-    # convert x/y -> ra/dec
-    print "pix2world"
-    pos = imwcs.all_pix2world(xy.T, 1)
-    ra = pos[:, 0]
-    dec = pos[:, 1]
-
-    # calculate the corrections in blocks of 100k since the rbf fails on large blocks
-    print 'applying corrections'
-    if len(ra) > 100000:
-        borders = range(0, len(ra)+1, 100000)
-        if borders[-1] != len(ra):
-            borders.append(len(ra))
-        for s1 in [slice(a, b) for a, b in zip(borders[:-1], borders[1:])]:
-            ra[s1] += dramodel(ra[s1], dec[s1]) * scale
-            dec[s1] += ddecmodel(ra[s1], dec[s1]) * scale
-    else:
-        ra += dramodel(ra, dec)
-        dec += ddecmodel(ra, dec)
-
-    # since ra/dec are views into pos, updaing ra/dec updates pos
-
-    print 'world2pix'
-    cxy = imwcs.all_world2pix(pos, 1)
-
-    print 'interpolating'
-    ifunc = LinearNDInterpolator(cxy, np.ravel(data))
-    interpolated_map = ifunc(xy[0, :], xy[1, :])
-
-    print 'writing'
-    interpolated_map.shape = data.shape
-    im[0].data = interpolated_map
-    im.writeto(fout, clobber=True)
-    print "wrote", fout
+    for fname in fnames:
+        fout = fname.replace(".fits", "_" + suffix + ".fits")
+        im = fits.open(fname)
+        data = np.squeeze(im[0].data)
+        print 'interpolating', fname
+        ifunc = CloughTocher2DInterpolator(np.transpose([x, y]), np.ravel(data))
+        interpolated_map = ifunc(xy[1, :], xy[0, :])
+        interpolated_map.shape = data.shape
+        # Float32 instead of Float64 since the precision is meaningless
+        im[0].data = np.array(interpolated_map, dtype=np.float32)
+        # NaN the edges by 10 pixels to avoid weird edge effects
+        im[0].data[0:10, :] = np.nan
+        im[0].data[:, 0:10] = np.nan
+        im[0].data[:, -10:im[0].data.shape[0]] = np.nan
+        im[0].data[-10:im[0].data.shape[1], :] = np.nan
+        im.writeto(fout, clobber=True)
+        print "wrote", fout
     return
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     group1 = parser.add_argument_group("input/output files")
     group1.add_argument("--xm", dest='xm', type=str, default=None,
-                        help='A .fits binary table. The crossmatch between the reference and source catalogue.')
+                        help='A .fits binary or VO table. The crossmatch between the reference and source catalogue.')
     group1.add_argument("--infits", dest='infits', type=str, default=None,
-                        help="The fits image that is to be corrected.")
-    group1.add_argument("--outfits", dest='outfits', type=str, default=None,
-                        help="The output (corrected) fits image.")
+                        help="The fits image(s) to be corrected; enclose in quotes for globbing.")
+    group1.add_argument("--suffix", dest='suffix', type=str, default=None,
+                        help="The suffix to append to rename the output (corrected) fits image(s); e.g., specifying \"warp\" will result in an image like image_warp.fits (no default; if not supplied, no correction will be performed).")
     group2 = parser.add_argument_group("catalog column names")
     group2.add_argument("--ra1", dest='ra1', type=str, default='ra',
                         help="The column name for ra  (degrees) for source catalogue.")
@@ -206,9 +229,9 @@ if __name__ == "__main__":
                         help="The column name for dec (degrees) for reference catalogue.")
     group3 = parser.add_argument_group("Other")
     group3.add_argument('--plot', dest='plot', default=False, action='store_true',
-                        help="Plot the ra/dec offset models")
-    group3.add_argument('--test', dest='test', default=False, action='store_true',
-                        help="run test")
+                        help="Plot the offsets and models (default = False)")
+    group3.add_argument('--smooth', dest='smooth', default=300.0, type=float,
+                        help="Smoothness parameter to give to the radial basis function (default = 300 pix)")
 
     results = parser.parse_args()
 
@@ -216,18 +239,13 @@ if __name__ == "__main__":
         parser.print_help()
         sys.exit()
 
-    if results.test:
-        dra, ddec = make_models('xmatched.fits', dec2='DECJ2000', plots=True)
-        correct_image('small.fits', dra, ddec, 'small_corr.fits')
-        sys.exit()
-
-    if results.infits is not None or results.plot:
-        dx, dy = make_pix_models(results.xm, results.ra1, results.dec1, results.ra2, results.dec2,
-                                 results.infits, results.plot)
     if results.infits is not None:
-        if os.path.exists(results.infits):
-            correct_image(results.infits, dx, dy, results.outfits)
-        else:
-            print "File:{0} not found".format(results.infits)
+        fnames = glob.glob(results.infits)
+        # Use the first image to define the model
+        dx, dy = make_pix_models(results.xm, results.ra1, results.dec1, results.ra2, results.dec2,
+                                 fnames[0], results.plot, results.smooth)
+    if results.suffix is not None:
+        # Correct all the images
+        correct_images(fnames, dx, dy, results.suffix)
     else:
-        print "No fits file supplied not doing warping"
+        print "No output fits file specified; not doing warping"
